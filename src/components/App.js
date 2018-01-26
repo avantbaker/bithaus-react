d78f6a322f85e5c2f2b5ec9af9fa5f3df69bcddf
Avant Baker <b.avant89@gmail.com>
2018-01-26 00:32:01 -0500

    More initial setup and started on the header

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="header">
+            <h1 className="title m-collapse">bithaüs</h1>
+            <a className="btn btn-navbar collapsed">
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+            </a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
